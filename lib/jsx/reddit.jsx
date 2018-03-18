24aaa1794589d1786183e6eb207d142cc9b5e520
danhab99 <dan.habot@gmail.com>
2018-03-18 21:56:52 +0200

    I works. Finally...

--- lib/jsx/reddit.jsx
+++ lib/jsx/reddit.jsx
@@ -1,6 +1,12 @@
+'use babel';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = class Reddit extends React.Component {
-
+    render() {
+        return (
+            <h1>Hello world</h1>
+        );
+    }
 };

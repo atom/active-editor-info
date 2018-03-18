b07426ee41bef0a6ed4bdcdd81492f81b74d0b20
danhab99 <dan.habot@gmail.com>
2018-03-18 22:09:16 +0200

    Cleanup

--- lib/jsx/reddit.jsx
+++ lib/jsx/reddit.jsx
@@ -4,6 +4,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = class Reddit extends React.Component {
+    constructor(props){
+        super(props);
+
+        
+    }
+
     render() {
         return (
             <h1>Hello world</h1>

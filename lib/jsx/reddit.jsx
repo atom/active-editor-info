2af8b5b90aeda50c3c216b953def7cccf2e608b6
danhab99 <dan.habot@gmail.com>
2018-03-28 18:22:52 +0300

    Cleaning up work tree

--- lib/jsx/reddit.jsx
+++ lib/jsx/reddit.jsx
@@ -35,7 +35,7 @@ class PostListItem extends React.Component {
         console.log("Rendered post item", this.props);
         return (<div id={this.props.id} className="PostItem grid-container">
             <div className="grid-item thumbnail">
-                <img src={this.props.thumbnail}/>
+                <img src={this.props.thumbnail} alt={this.props.thumbnail}/>
             </div>
             <div className="grid-item title">
                 <h2>{this.props.title}</h2>
@@ -76,7 +76,7 @@ class PostList extends React.Component {
         console.log("Rendered post list", this.props);
         var list = this.props.list.map(post => {
             return (<li>
-                <PostListItem id={post.id} thumbnail={post.thumbnail} title={post.title} author={post.author}></PostListItem>
+                <PostListItem id={post.id} thumbnail={post.thumbnail} title={post.title} author={post.author} votes={post.votes}></PostListItem>
             </li>)
         });
 
@@ -98,7 +98,9 @@ module.exports = class Reddit extends React.Component {
         redditAuth(props).then(snoowrap => {
             snoowrap.getHot().then(posts => {
                 console.log("Downloaded hot", posts.map(x => x.title));
-                this.setState({'list': posts}, () => {
+                this.setState({
+                    'list': posts
+                }, () => {
                     console.log("Reddit state altered");
                     this.forceUpdate();
                 });
